7cddd7b62ca6a3671ae7bee241585aaf74a7c8d2
Eman-Bakri <eman1996-sy@hotmail.com>
2024-01-29 12:01:47 -0800

    export csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,13 +5,13 @@ import requests as r
 import sys
 
 if __name__ == "__main__":
-    empl_id = sys.argv[1]
+    user_id = sys.argv[1]
     link = "https://jsonplaceholder.typicode.com/"
-    empl = r.get(link + "users/{}".format(user_id)).json()
-    username = empl.get("username")
-    to_do = r.get(link + "todos", params={"userId": empl_id}).json()
+    usr = r.get(link + "users/{}".format(user_id)).json()
+    username = usr.get("username")
+    to_do = r.get(link + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(empl_id), "w", newline="") as csvfile:
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow([empl_id, username, elm.get("completed"),
+        [writer.writerow([user_id, username, elm.get("completed"),
                           elm.get("title")]) for elm in to_do]

7f197e7ab855ec059d41ea1adaddb392efa27006
Eman-Bakri <eman1996-sy@hotmail.com>
2024-01-29 12:32:09 -0800

    employee info

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,12 +5,11 @@ import sys
 
 if __name__ == '__main__':
     link = 'https://jsonplaceholder.typicode.com/'
-    empl_id = r.get(link + 'users/{}'.format(sys.argv[1])).json()
+    user_id = r.get(link + 'users/{}'.format(sys.argv[1])).json()
     to_do = r.get(link + 'todos', params={'userId': sys.argv[1]}).json()
-    is_done = [title.get("title") for title in to_do if
+    completed = [title.get("title") for title in to_do if
                  title.get('completed') is True]
-    print(is_done)
-    print("Employee {} is done with tasks({}/{}):".format(empl_id.get("name"),
-                                                          len(is_done),
+    print("Employee {} is done with tasks({}/{}):".format(user_id.get("name"),
+                                                          len(completed),
                                                           len(to_do)))
-    [print("\t {}".format(title)) for title in is_done]
+    [print("\t {}".format(title)) for title in completed]

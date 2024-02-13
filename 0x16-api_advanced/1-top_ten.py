06acc3c7b5816933f760606aef07a700febaf2cc
Eman-Bakri <41126091+Eman-Bakri@users.noreply.github.com>
2024-02-13 19:37:44 +0300

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Function that print first 10 titles on a Reddit API"""
-import requests
 
 
 def top_ten(subreddit):
     """Print the 10 hottest posts titles."""
+    import requests
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
